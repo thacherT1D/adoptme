7b2dbfd13d111a77533b9a833dad2cfa63f4781d
THACHER <sophie.thacher@gmail.com>
2016-06-30 19:54:24 -0700

    mapped to natgeoreddit

--- available/available.controller.js
+++
@@ -1,8 +0,0 @@
-angular.module('adoptMe')
-  .controller('availableController', function($scope) {
-    $scope.doSomething = function() {
-      window.alert('I did something!');
-      document.createElement("div");
-      angular.element(document.getElementsByTagName('body')).append( $scope.doSomething);
-    };
-  });
